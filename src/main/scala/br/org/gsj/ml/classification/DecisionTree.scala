ab94f7149a9aee7a0cc4f919573aa6fd8ee28bc5
Geraldo <gsjunior@mail.uni-paderborn.de>
2018-06-27 16:19:43 +0200

    finished the Decision tree Implementation

--- src/main/scala/br/org/gsj/ml/classification/DecisionTree.scala
+++ src/main/scala/br/org/gsj/ml/classification/DecisionTree.scala
@@ -4,6 +4,13 @@ import org.apache.spark.sql.SparkSession
 
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.Row
+import org.apache.spark.ml.classification.DecisionTreeClassifier
+import org.apache.spark.sql.functions._
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.classification.DecisionTreeClassificationModel
+
 
 object DecisionTree {
   
@@ -16,16 +23,17 @@ object DecisionTree {
     
     val sc = spark.sparkContext
     
+    import spark.implicits._
+    
     val zoo_rdd = sc.textFile("src/main/resources/datasets/zoo.data")
     .map(x=>x.split(","))    
-    .map(row => Row(row(0).toString(),row(1).toInt,row(2).toInt,row(3).toInt,row(4).toInt,row(5).toInt
+    .map(row => Row(row(1).toInt,row(2).toInt,row(3).toInt,row(4).toInt,row(5).toInt
         ,row(6).toInt,row(7).toInt,row(8).toInt,row(9).toInt,row(10).toInt,row(11).toInt,row(12).toInt,row(13).toInt
         ,row(14).toInt,row(15).toInt,row(16).toInt,row(17).toInt))
     
     
     val schema = StructType(
           Array(
-                StructField("animal_name", StringType, false),
                 StructField("hair", IntegerType, false),
                 StructField("feathers", IntegerType, false),
                 StructField("eggs", IntegerType, false),
@@ -46,11 +54,42 @@ object DecisionTree {
               )
         )
         
+        val dt = new DecisionTreeClassifier()
+          .setLabelCol("Label")
+          .setFeaturesCol("Features")
+        
+        var zoo_df = spark.createDataFrame(zoo_rdd, schema)
+
+            
+        val va = new VectorAssembler().setOutputCol("Features").
+            setInputCols(Array("hair","feathers","eggs","milk","airborne","aquatic",
+                "predator","toothed","backbone","breathes","venomous","fins","legs","tail","domestic","catsize"))
+            
+        val zoo_labeled = va.transform(zoo_df).select("Features","type").withColumnRenamed("type", "Label")
+         
+        // Split the data into training and test sets (30% held out for testing).
+        val Array(trainingData, testData) = zoo_labeled.randomSplit(Array(0.7, 0.3))
+        
+        val pipeline = new Pipeline().setStages(Array(dt))
+        
+        val model = pipeline.fit(zoo_labeled)
         
         
-        val zoo_df = spark.createDataFrame(zoo_rdd, schema)
+        val predictions = model.transform(testData)
         
-        zoo_df.show()
+        // rows to display.
+        predictions.show
+        
+        // Select (prediction, true label) and compute test error.
+        val evaluator = new MulticlassClassificationEvaluator()
+          .setLabelCol("Label")
+          .setPredictionCol("prediction")
+          .setMetricName("accuracy")
+        val accuracy = evaluator.evaluate(predictions)
+        println(s"Test Error = ${(1.0 - accuracy)}")
+        
+        val treeModel = model.stages(0).asInstanceOf[DecisionTreeClassificationModel]
+        println(s"Learned classification tree model:\n ${treeModel.toDebugString}")
     
   }
   
