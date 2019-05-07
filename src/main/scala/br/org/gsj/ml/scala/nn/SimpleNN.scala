e01683bc33e6999ba6cf0066fc6478e0d4e1c77e
Geraldo <gsjunior86@gmail.com>
2019-05-08 00:38:15 +0200

    updated nn

--- src/main/scala/br/org/gsj/ml/scala/nn/SimpleNN.scala
+++ src/main/scala/br/org/gsj/ml/scala/nn/SimpleNN.scala
@@ -2,13 +2,10 @@ package br.org.gsj.ml.scala.nn
 
 import scala.math._
 
-object SimpleNN {
+case class SimpleNN(val w1: Double, val w2: Double, val bias: Double){
   
-  private val w1 : Double = .5
-  private val w2 : Double = .2
-  private val bias : Double = .3
-  
-  def compute(m1: Double, m2: Double): Double ={
+
+  def apply(m1: Double, m2: Double): Double ={
     sigmoid(w1*m1+w2*m2+bias)
   }
   
--- src/main/scala/br/org/gsj/ml/scala/nn/SimpleNNTest.scala
+++ src/main/scala/br/org/gsj/ml/scala/nn/SimpleNNTest.scala
@@ -2,8 +2,32 @@ package br.org.gsj.ml.scala.nn
 
 object SimpleNNTest {
   
+  /**
+   * 
+   * Flower Classification
+   * 
+   * length, width , color
+   * 
+   */
+
   def main(args: Array[String]): Unit = {
-    println(SimpleNN.compute(2, 1))
+
+    val data = List((3D, 1.5, 1D), (2D, 1D, 0D), (4D, 1.5, 1D), (3D, 1D, 0D),
+      (3.5, .5, 1D), (2D, .5, 0D), (5.5, 1D, 1D), (1D, 1D, 0D))
+
+    val w1 = scala.util.Random.nextDouble() * 2 - 1
+    val w2 = scala.util.Random.nextDouble() * 2 - 1
+    val bias = scala.util.Random.nextDouble() * 2 - 1
+
+    println("w1: " + w1)
+    println("w2: " + w2)
+    println("bias: " + bias)
+
+    for (b <- data) {
+      println("Classficiation: " + SimpleNN(w1, w2, bias)(b._1, b._2))
+      println()
+    }
+
   }
-  
+
 }
