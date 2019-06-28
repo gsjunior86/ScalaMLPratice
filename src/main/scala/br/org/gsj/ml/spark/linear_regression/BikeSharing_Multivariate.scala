58a7e3eaa06345dcd6cc47f8a46d1808dc7fbffb
Geraldo <gsjunior86@gmail.com>
2019-06-28 16:07:42 +0200

    finished multivariate lr

--- src/main/scala/br/org/gsj/ml/spark/linear_regression/BikeSharing_Multivariate.scala
+++ src/main/scala/br/org/gsj/ml/spark/linear_regression/BikeSharing_Multivariate.scala
@@ -4,6 +4,8 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 import scala.collection.mutable.ListBuffer
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.regression.LinearRegression
 
 object BikeSharing_Multivariate {
 
@@ -37,20 +39,46 @@ object BikeSharing_Multivariate {
     val independent_variables = Array("season", "yr", "mnth", "holiday",
       "weekday", "workingday", "weathersit", "temp", "atemp",
       "hum", "windspeed")
-      
+
     val dependent_variable = Array("cnt")
-    
+
     val bike_sharing_df = bsDay_df.select(independent_variables.union(dependent_variable).map(col): _*)
-    
+
     val listRes = new ListBuffer[String]
 
     for (i <- bike_sharing_df.columns) {
-      listRes+=("Correlation to CNT for: " + i + ", " + bsDay_df.stat.corr("cnt", i))
+      listRes += ("Correlation to CNT for: " + i + ", " + bsDay_df.stat.corr("cnt", i))
     }
-    
+
     listRes.foreach(println)
 
-//    bsDay_df.printSchema()
+    val multivariate_feature_columns = Array("season", "yr", "mnth", "temp", "atemp")
+    val multivariate_label_column = Array("cnt")
+
+    val vector_assembler = new VectorAssembler()
+      .setInputCols(multivariate_feature_columns)
+      .setOutputCol("features")
+
+    val bike_sharing_features_df = vector_assembler
+      .transform(bike_sharing_df)
+      .select("features", multivariate_label_column(0))
+
+    bike_sharing_features_df.show
+
+    val train_test_array_ds = bike_sharing_features_df.randomSplit(Array(0.75, 0.25), 12345)
+
+    val linear_regression = new LinearRegression()
+      .setFeaturesCol("features")
+      .setLabelCol(multivariate_label_column(0))
+
+    val linear_regression_model = linear_regression.fit(train_test_array_ds(0))
+
+    println("Model Coefficients: " + linear_regression_model.coefficients)
+    println("Intercept: " + linear_regression_model.intercept)
+    val training_summary = linear_regression_model.summary
+    println("RMSE:" + training_summary.rootMeanSquaredError)
+    println("R-SQUARED: " + training_summary.r2)
+    
 
   }
 
