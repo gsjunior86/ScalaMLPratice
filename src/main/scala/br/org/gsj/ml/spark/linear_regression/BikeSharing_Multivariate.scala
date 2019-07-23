ed35bbcd25436e7422a6cc8f0f9d115c9f54ee59
gsjunior <gsjunior@localhost>
2019-07-23 23:15:42 +0200

    added multivariate linear regression

--- src/main/scala/br/org/gsj/ml/spark/linear_regression/BikeSharing_Multivariate.scala
+++ src/main/scala/br/org/gsj/ml/spark/linear_regression/BikeSharing_Multivariate.scala
@@ -3,11 +3,14 @@ package br.org.gsj.ml.spark.linear_regression
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.regression.LinearRegression
+import org.apache.spark.ml.evaluation.RegressionEvaluator
 
 object BikeSharing_Multivariate {
-  
+
   def main(args: Array[String]): Unit = {
-    
+
     val spark = SparkSession.builder()
       .appName("bikeSharing-spark")
       .master("local[*]")
@@ -33,10 +36,74 @@ object BikeSharing_Multivariate {
       .withColumn("registered", col("registered").cast(IntegerType))
       .withColumn("cnt", col("cnt").cast(IntegerType))
 
-      
+    val independent_variables = Array("season", "yr", "mnth", "holiday",
+      "weekday", "workingday", "weathersit", "temp", "atemp", "hum", "windspeed")
+      .map(f => col(f))
 
-       bsDay_df.printSchema()
+    val dependent_variable = Array("cnt").map(f => col(f))
+
+    val bike_sharing_df = bsDay_df.select(independent_variables ++ dependent_variable: _*)
+    bike_sharing_df.show()
+
+    for (i <- bike_sharing_df.columns) {
+      println("Correlation to CNT for: " + i + ": " + bike_sharing_df.stat.corr("cnt", i))
+    }
+
+    val multivariate_feature_columns = Array("season", "yr", "mnth", "temp", "atemp")
+    val multivariate_label_column = "cnt"
+
+    val vector_assembler = new VectorAssembler()
+      .setInputCols(multivariate_feature_columns)
+      .setOutputCol("features")
+
+    val bike_sharing_df_features = vector_assembler
+      .transform(bike_sharing_df)
+      .select(col("features"), col(multivariate_label_column))
+
+    bike_sharing_df_features.show()
+
+    val prediction_df = bike_sharing_df_features.randomSplit(Array(0.75, 0.25))
+    println(prediction_df(0).count)
+    println(prediction_df(1).count)
+
+    val linear_regression = new LinearRegression()
+      .setFeaturesCol("features")
+      .setLabelCol(multivariate_label_column)
+
+    val linear_regression_model = linear_regression.fit(prediction_df(0))
+
+    println("Model Coefficients: " + linear_regression_model.coefficients)
+    println("Intercept: " + linear_regression_model.intercept)
+    val training_summary = linear_regression_model.summary
+    println("RMSE:" + training_summary.rootMeanSquaredError)
+    println("R-SQUARED: " + training_summary.r2)
     
+    println("TRAINING DATASET DESCRIPTIVE SUMMARY: ")
+    prediction_df(0).describe().show()
+    print("TRAINING DATASET RESIDUALS: ")
+    training_summary.residuals.show
+    
+    val test_linear_regression_predictions_df = linear_regression_model.transform(prediction_df(1))
+    test_linear_regression_predictions_df
+      .select("prediction", multivariate_label_column, "features")
+      .show(10)
+      
+      val linear_regression_evaluator_rmse = new RegressionEvaluator()
+    .setPredictionCol("prediction")
+    .setLabelCol(multivariate_label_column)
+    .setMetricName("rmse")
+    
+    val linear_regression_evaluator_r2 = new RegressionEvaluator()
+    .setPredictionCol("prediction")
+    .setLabelCol(multivariate_label_column)
+    .setMetricName("r2")
+    
+    val rmse = linear_regression_evaluator_rmse.evaluate(test_linear_regression_predictions_df)
+    val r2 = linear_regression_evaluator_r2.evaluate(test_linear_regression_predictions_df)
+    
+    println("RMSE on Test Data: " + rmse)
+    println("R2 on Test Data: " + r2)
+
   }
-  
+
 }
