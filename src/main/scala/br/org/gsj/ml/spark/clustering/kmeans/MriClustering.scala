f9fdaa9b50661becad5552fa8c82ec949aa153b7
Geraldo <gsjunior86@gmail.com>
2019-09-20 11:59:49 +0200

    changed target image name

--- src/main/scala/br/org/gsj/ml/spark/clustering/kmeans/MriClustering.scala
+++ src/main/scala/br/org/gsj/ml/spark/clustering/kmeans/MriClustering.scala
@@ -27,7 +27,10 @@ object MriClustering {
         
     import spark.implicits._
     
+    
     val image_df = spark.sparkContext.parallelize(image_array).map(f => Image(f._1,f._2)).toDF
+    
+    image_df.show()
           
     val features_col = Array("data")
     val vector_assembler = new VectorAssembler()
@@ -40,6 +43,7 @@ object MriClustering {
     val kmeans = new KMeans().setK(k).setSeed(12345).setFeaturesCol("features")
     val kmeans_model = kmeans.fit(mri_healthy_brain_df)   
     val kmeans_centers = kmeans_model.clusterCenters
+    
     println("Cluster Centers --------")
     for(k <- kmeans_centers)
       println(k)
@@ -56,7 +60,7 @@ object MriClustering {
     val photo1 = ImageIO.read(new File(mri_healthy_brain_image))
     val photo2 = ImageUtils.generateImage(photo1, image_array_final)
     
-    ImageIO.write(photo2, "jpg", new File("src/main/resources/datasets/clustering/data/mri-images-data/mri-healthy-test.png"))
+    ImageIO.write(photo2, "jpg", new File("src/main/resources/datasets/clustering/data/mri-images-data/mri-test-result.png"))
 
 
 
